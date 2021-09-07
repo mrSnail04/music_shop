2e27a3eb40944980adaddcd2ee601c96c955992a
Maksim Bulauski <maks.bulavsky@gmail.com>
2021-09-07 19:32:17 +0300

    Добавлены страницы главная, артист и альбом. Добавлены эндпоинты для страниц артист и альбом. Подключил бутстрап5.

--- music_shop/musicshop_app/models.py
+++ music_shop/musicshop_app/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 from django.conf import settings
 from django.utils.safestring import mark_safe
 from utils import upload_function
+from django.urls import reverse
 
 
 class MediaType(models.Model):
@@ -61,6 +62,9 @@ class Artist(models.Model):
     def __str__(self):
         return f"{self.name} | {self.genre.name}"
 
+    def get_absolute_url(self):
+        return reverse('artist_detail', kwargs={'artist_slug': self.slug})
+
     class Meta:
         verbose_name = 'Исполнитель'
         verbose_name_plural = 'Исполнители'
@@ -83,6 +87,9 @@ class Album(models.Model):
 
     def __str__(self):
         return f"{self.id} | {self.artist.name} | {self.name}"
+
+    def get_absolute_url(self):
+        return reverse('album_detail', kwargs={'artist_slug': self.artist.slug, 'album_slug': self.slug})
 
     @property
     def ct_model(self):
--- music_shop/musicshop_app/views.py
+++ music_shop/musicshop_app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django import views
+from .models import Artist, Album
 
-# Create your views here.
+
+class BaseView(views.View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'base.html', {})
+
+
+class ArtistDetailView(views.generic.DeleteView):
+
+    model = Artist
+    template_name = 'artist/artist_detail.html'
+    slug_url_kwarg = 'artist_slug'
+    context_object_name = 'artist'
+
+
+class AlbumDetailView(views.generic.DeleteView):
+
+    model = Album
+    template_name = 'album/album_detail.html'
+    slug_url_kwarg = 'album_slug'
+    context_object_name = 'album'
